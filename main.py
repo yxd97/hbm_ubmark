38b7c6ea5d37020cdf99731576c1f0c667057d8b
yxd97 <yd383@cornell.edu>
2023-06-02 18:30:20 -0400

    makefile generation seems good

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import sys
+import argparse
 from typing import List,Tuple
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-from utils.syscfg_utils import *
+from utils import syscfg_utils
+from utils import makefile_utils
 
 CONNECTIVITIES = [
     'all2all',
@@ -31,8 +32,11 @@ class TestCase:
         self.ntg = ntg
         self.nch = nch
         self.name = f'{connectivity}_{ntg}t{nch}c'
-        abs_root_path = os.path.abspath(os.path.dirname(__file__))
-        self.build_dir = os.path.join(abs_root_path, 'build_' + self.name)
+        self.include_dir = 'include'
+        self.project_root = os.path.join('.', self.name)
+        self.build_dir = os.path.join(self.project_root, 'build')
+        self.output_dir = os.path.join(self.project_root, 'output')
+        self.floorplan = None
 
     def apply_floorplan(self, ntg_slrs:Tuple[int, int, int]):
         if len(ntg_slrs) != 3:
@@ -43,30 +47,40 @@ class TestCase:
 
     def create_sys_cfg(self):
         with open(os.path.join(self.build_dir, 'system.cfg'), 'w', newline='') as f:
-            f.write(f"# Testcase: {self.name}")
+            f.write(f"# Testcase: {self.name}\n")
             f.write('[connectivity]\n')
-            f.writelines(generate_nk_tags(self.ntg))
+            f.writelines(syscfg_utils.generate_nk_tags(self.ntg))
             if self.floorplan is not None:
-                f.writelines(generate_slr_tags(self.floorplan))
-            f.writelines(generate_sp_tags(self.ntg, self.nch, self.connectivity))
+                f.writelines(syscfg_utils.generate_slr_tags(self.floorplan))
+            f.writelines(syscfg_utils.generate_sp_tags(self.ntg, self.nch, self.connectivity))
 
-    def create_makefile(self):
+    def create_makefile(self, board_repo_path:str, board_xsa:str):
         with open(os.path.join(self.build_dir, 'Makefile'), 'w', newline='') as f:
-            f.write(f"# Testcase: {self.name}")
+            f.write(f"# Testcase: {self.name}\n")
+            f.writelines(makefile_utils.generate_baisc_config(board_repo_path, board_xsa))
+            f.writelines(makefile_utils.generate_include_oclxcl(self.include_dir))
+            f.writelines(makefile_utils.generate_dir_config(self.build_dir, self.output_dir))
+            f.writelines(makefile_utils.generate_kernel_config('hbm_tg'))
+            f.writelines(makefile_utils.generate_host_config(self.include_dir))
+            f.writelines(makefile_utils.generate_emulation_config())
+            f.writelines(makefile_utils.generate_build_targets(self.name))
+            f.writelines(makefile_utils.generate_run_targets())
+            f.writelines(makefile_utils.generate_clean_targets())
 
     def create_host_cpp(self):
         with open(os.path.join(self.build_dir, 'host.cpp'), 'w', newline='') as f:
-            f.write(f"// Testcase: {self.name}")
+            f.write(f"// Testcase: {self.name}\n")
 
-    def create_build_files(self):
-        pinfo(f'Creating build files in {self.build_dir}...')
+    def create_build_files(self, board_repo_path:str, board_xsa:str):
+        pinfo(f'Creating build files in {self.build_dir} ')
         os.makedirs(self.build_dir, exist_ok=True)
+        os.makedirs(self.output_dir, exist_ok=True)
         self.create_sys_cfg()
-        self.create_makefile()
+        self.create_makefile(board_repo_path, board_xsa)
         self.create_host_cpp()
 
     def build_test(self, no_host:bool=False):
-        pinfo(f'Building test {self.name}...')
+        pinfo(f'Building test {self.name} ')
         os.chdir(self.build_dir)
         if not no_host:
             os.system('make xclbin host')
@@ -74,21 +88,55 @@ class TestCase:
             os.system('make xclbin')
 
     def run_test(self):
-        pinfo(f'Running test {self.name}...')
+        pinfo(f'Running test {self.name} ')
         os.chdir(self.build_dir)
         os.system('make run')
 
+    def clean_logs(self):
+        pinfo(f'Cleaning logs for {self.name} ')
+        os.chdir(self.build_dir)
+        os.system('make clean')
+
+    def delete_build(self):
+        pinfo(f'Cleaning build temp files for {self.name} ')
+        os.chdir(self.build_dir)
+        os.system('make cleanall')
+
+    def delete_all(self):
+        pinfo(f'Deleting all files for {self.name} ')
+        os.system(f'rm -rf {self.project_root}')
+
+#TODO: move them to a separate file (maybe json?)
 WORK_DISTRIBUTION = {
-    'zhang-21.ece.cornell.edu' : [TestCase('all2all', 32, 32),      TestCase('one2one', 32, 32)] ,
-    'zhang-22.ece.cornell.edu' : [TestCase('grp4_all2all', 32, 32), TestCase('grp4_all2all', 16, 32)],
-    'zhang-23.ece.cornell.edu' : [TestCase('grp8_all2all', 32, 32), TestCase('grp8_all2all', 16, 32)],
-    'zhang-24.ece.cornell.edu' : [TestCase('one2all', 1, 1),        TestCase('one2all', 1, 4), TestCase('one2all', 1, 8), TestCase('one2all', 1, 16), TestCase('one2all', 1, 32)],
+    'zhang-24.ece.cornell.edu' : [
+        TestCase('all2all', 32, 32),      TestCase('one2one', 32, 32) ,
+        TestCase('grp4_all2all', 32, 32), TestCase('grp4_all2all', 16, 32),
+        TestCase('grp8_all2all', 32, 32), TestCase('grp8_all2all', 16, 32),
+        TestCase('one2all', 1, 1),        TestCase('one2all', 1, 4), TestCase('one2all', 1, 8), TestCase('one2all', 1, 16), TestCase('one2all', 1, 32)
+    ],
 }
+#TODO: move them to a separate file (maybe json?)
+BORAD_REPO_PATH = '/work/shared/common/CAD_tool/xilinx/platforms/'
+U280_XSA = 'xilinx_u280_xdma_201920_3'
+
 
 def get_hostname():
     return os.uname()[1]
 
 def main():
+    parser = argparse.ArgumentParser(description='Test case generator')
+    parser.add_argument(
+        'option', type=str,
+        choices=['setup', 'build', 'run', 'clean', 'cleanall', 'delete'],
+        help = "setup: create build files; "
+               "build: build the test case; "
+               "run: run the test case; "
+               "clean: clean the logs; "
+               "cleanall: clean the logs and build temp files; "
+               "delete: delete the test cases' outputs; "
+    )
+    args = parser.parse_args()
+
     hostname = get_hostname()
     if hostname not in WORK_DISTRIBUTION:
         perror(f'Unknown hostname: {hostname}. Please add it in the WORK_DISTRIBUTION dictionary.')
@@ -97,11 +145,22 @@ def main():
     test_cases = WORK_DISTRIBUTION[hostname]
     for test_case in test_cases:
         try:
-            test_case.create_build_files()
-            test_case.build_test()
-            # test_case.run_test()
+            if args.option == 'setup':
+                if test_case.ntg == 32:
+                    test_case.apply_floorplan((10,11,11))
+                test_case.create_build_files(BORAD_REPO_PATH, U280_XSA)
+            elif args.option == 'build':
+                test_case.build_test()
+            elif args.option == 'run':
+                test_case.run_test()
+            elif args.option == 'clean':
+                test_case.clean_logs()
+            elif args.option == 'cleanall':
+                test_case.delete_build()
+            elif args.option == 'delete':
+                test_case.delete_all()
         except Exception as e:
-            perror(f'Failed to build test case {test_case.name} with exception: {e}')
+            perror(f'Failed to execute "{args.option}" on test case {test_case.name} with exception: {e}')
 
 if __name__ == '__main__':
     main()
--- utils/makefile_utils.py
+++ utils/makefile_utils.py
@@ -1,6 +1,6 @@
 from typing import List,Tuple
 
-def generate_makefile_baisc_config(
+def generate_baisc_config(
         board_repo_path:str,
         board_xsa_name:str,
         cpp_compiler:str='/usr/bin/g++',
@@ -17,22 +17,24 @@ def generate_makefile_baisc_config(
         '\n',
     ]
 
-def generate_makefile_include_oclxcl(include_dir:str) -> List[str]:
+def generate_include_oclxcl(include_dir:str) -> List[str]:
     return [
         f'include {include_dir}/opencl/opencl.mk\n',
         f'include {include_dir}/xcl2/xcl2.mk\n',
+        '\n',
     ]
 
-def generate_makefile_dir_config(build_dir:str, output_dir:str) -> List[str]:
+def generate_dir_config(build_dir:str, output_dir:str) -> List[str]:
     return [
-        f'BUILD_DIR := ./{build_dir}\n',
-        f'OUTPUT_DIR := ./{output_dir}\n',
+        f'BUILD_DIR := {build_dir}\n',
+        f'OUTPUT_DIR := {output_dir}\n',
         'LOG_DIR := $(BUILD_DIR)/logs\n',
         'COMPILE_DIR := $(BUILD_DIR)/compile_$(TARGET)\n',
         'LINK_DIR := $(BUILD_DIR)/link_$(TARGET)\n',
+        '\n',
     ]
 
-def generate_makefile_kernel_config(kernel_name:str, target_frequency_mhz:int=300) -> List[str]:
+def generate_kernel_config(kernel_name:str, target_frequency_mhz:int=300) -> List[str]:
     return [
         f'KRNL_NAME := {kernel_name}\n',
         'KRNL_SRC := ../$(KRNL_NAME)/$(KRNL_NAME).cpp\n',
@@ -46,7 +48,7 @@ def generate_makefile_kernel_config(kernel_name:str, target_frequency_mhz:int=30
         '\n',
     ]
 
-def generate_makefile_host_config(include_dir:str) -> List[str]:
+def generate_host_config(include_dir:str) -> List[str]:
     return [
         'HOST_SRC := host.cpp\n',
         'HOST_EXE := host.exe\n',
@@ -58,9 +60,10 @@ def generate_makefile_host_config(include_dir:str) -> List[str]:
         'HOST_LINK_OPTS := $(xcl2_LDFLAGS)\n',
         'HOST_LINK_OPTS += $(opencl_LDFLAGS)\n',
         'HOST_LINK_OPTS += -lOpenCL -lxrt_coreutil -lrt\n',
+        '\n',
     ]
 
-def generate_makefile_emulation_config(
+def generate_emulation_config(
     emuconfig_util_binary:str='$(XILINX_VITIS)/bin/emconfigutil',
 ) -> List[str]:
     return [
@@ -70,7 +73,7 @@ def generate_makefile_emulation_config(
         '\n',
     ]
 
-def generate_makefile_build_targets(test_case_name:str) -> List[str]:
+def generate_build_targets(test_case_name:str) -> List[str]:
     return [
         '.PHONY: xclbin host emconfig\n',
         'xclbin: $(FPGA_BINARY)\n',
@@ -89,24 +92,27 @@ def generate_makefile_build_targets(test_case_name:str) -> List[str]:
         '\n',
         '$(EMCONFIG_FILE):\n',
         '\t$(EMCONFIGUTIL) --platform $(PLATFORM)\n',
+        '\n',
     ]
 
-def generate_makefile_clean_targets() -> List[str]:
+def generate_clean_targets() -> List[str]:
     return [
         '.PHONY: clean cleanall\n',
         'clean:\n',
-        '\trm -rf $(EMCONFIG_FILE) $(HOST_EXE)\n',
+        '\trm -rf $(EMCONFIG_FILE)\n',
         '\trm -rf *.log *.jou\n',
         '\trm -rf .run\n',
         '\n',
         'cleanall: clean\n',
-        '\trm -rf $(BUILD_DIR) $(OUTPUT_DIR)/*\n',
+        '\trm -rf  $(HOST_EXE) $(BUILD_DIR) $(OUTPUT_DIR)/*\n',
         '\trm -rf .Xil .ipcache\n',
+        '\n',
     ]
 
-def generate_makefile_run_targets() -> List[str]:
+def generate_run_targets() -> List[str]:
     return [
         '.PHONY: run\n',
         'run: $(HOST_EXE) $(FPGA_BINARY)\n',
         '\t./$(HOST_EXE) $(FPGA_BINARY)\n',
+        '\n',
     ]
